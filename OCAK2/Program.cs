35e5ee53747a90fdeca95db3da6284717cbd4e35
betularass <betularas16@gmail.com>
2025-03-15 17:45:45 +0300

    OCAK2

---
+++ OCAK2/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+// 1.SORU
+
+/*C#'ta yaygın olarak kullanılan temel veri türleri şunlardır:
+
+Tam Sayılar (Integer Types)
+
+byte
+sbyte
+short
+ushort
+int
+uint
+long
+ulong
+
+
+float
+double
+decimal
+
+
+char
+string
+Mantıksal Veri Tipi
+
+bool */
+
+
+
+// 2. SORU
+
+
+ /* 
+  int x = 5;
+intveri tipi4 bayt (32 bit) yer
+Sabit bir bellek kullanımı vardır ve yığın bellekte saklanır.
+
+    double ve8 bayt (64 bit)Yerinde
+Ondalığın saklandığı için int'
+Sabit bir bellek kullanımına sahiptir ve stack bellekte
+
+    mehmet 6 bitlik karakter içerir
+
+    6 * 2 = 12 byte karakter var*/
+
+   
+
+     // 3.SORU
+
+
+/*
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        double toplam = sayi1 + sayi2;
+
+        Console.WriteLine("Toplam: " + toplam);
+    }
+*/
